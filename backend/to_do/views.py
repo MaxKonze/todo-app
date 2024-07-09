45e3f7d7abb0678b666300877f75cf3c2ef13ab4
Maximilian <konmax1801@gho365.de>
2024-07-09 18:38:16 +0000

    first input

--- backend/to_do/views.py
+++ backend/to_do/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+output = ''
+
 
 def to_dos(request):
+    global output
+
+    if request.method == 'POST':
+        output = (request.POST['new_task'])
+        print(output)
+
     return render(request, 'to_do_list.html')
